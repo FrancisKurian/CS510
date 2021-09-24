fc8b859c42fafc509a238c8b61b4162f867de52f
FrancisKurian <kurian@chapman.edu>
2021-09-24 14:22:23 -0700

    csv reading

---
+++ code/Mid Term Project Code1.R
@@ -0,0 +1,6 @@
+# Mid Term Project
+# Read CSV file from data folder using the company name
+# Do data exploration, apply data cleaning issues, do basic plots to understand
+df_stock <- read.csv(file='C:\\Users\\jeoku\\OneDrive\\Documents\\chapman\\CS510\\data\\kaggle_historic_stock_info.csv',header=TRUE)
+
+df_stock[2,]

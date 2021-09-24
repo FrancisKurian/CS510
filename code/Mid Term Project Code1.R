8294841808cf0f6328a11629dca7967df5698eb9
FrancisKurian <kurian@chapman.edu>
2021-09-24 16:58:22 -0700

    viridis updated

--- code/Mid Term Project Code1.R
+++ code/Mid Term Project Code1.R
@@ -1,6 +1,21 @@
 # Mid Term Project
 # Read CSV file from data folder using the company name
 # Do data exploration, apply data cleaning issues, do basic plots to understand
-df_stock <- read.csv(file='C:\\Users\\jeoku\\OneDrive\\Documents\\chapman\\CS510\\data\\kaggle_historic_stock_info.csv',header=TRUE)
 
+df_stock <- read.csv(file='C:\\Users\\jeoku\\OneDrive\\Documents\\chapman\\CS510\\data\\kaggle_historic_stock_info.csv',header=TRUE)
 df_stock[2,]
+
+myvec <- c(3:12)
+myvec
+mymat <- matrix(c(1:10),5,2)
+mymat
+
+
+if (any(myvec-1)>9)||matrix(myvec,2,5)[2,1]<=6{
+  
+}
+
+if ((any(myvec-1))>9 || matrix(myvec,2,5)[2,1]<=6){
+  matrix(myvec,2,5)
+  
+}
--- code/line_plots_v1.R
+++ code/line_plots_v1.R
@@ -1,14 +1,25 @@
+
+## Simple Scatter Plot with colors for each tree
 data(Orange)
 
-#Simple Scatter Plot with colors for each tree
-
 plot(Orange$age,Orange$circumference,xlab="Age (days)",ylab="Circumference (mm)",pch=19,col=Orange$Tree)
+
 legend('topleft',sort(levels(Orange$Tree)),col=1:length(levels(Orange$Tree)),pch=19)
 
 
+## Trials with viridis
 
-#GGPLOT solution from:
-#https://lost-stats.github.io/Presentation/Figures/line_graphs.html
+library(viridis)
+
+Orange$Tree <- factor(Orange$Tree, levels=seq(1,nlevels(Orange$Tree)),ordered=T)
+tree.colors <- viridis(nlevels(Orange$Tree))
+
+plot(x=Orange$age,y=Orange$circumference, type ='p',pch=19, col=tree.colors[Orange$Tree])
+
+legend('topleft',legend=levels(Orange$Tree),col=tree.colors,pch=19)
+
+
+## GGPLOT solution from:
 library(ggplot2) 
 
 ggplot(Orange, aes(x = age, y = circumference, color = Tree)) + 
@@ -16,10 +27,5 @@ ggplot(Orange, aes(x = age, y = circumference, color = Tree)) +
   theme(plot.title = element_text(hjust = 0.5))
 
 
-#Trials with viridis
 
-library(viridis)
-tree.colors <- viridis(nlevels(Orange$Tree))
 
-plot(x=Orange$age,y=Orange$circumference, type ='l',col=Orange$Tree)
-legend('topleft',sort(levels(Orange$Tree)),col=1:length(levels(Orange$Tree)),pch=19)
